2e7a2fd12f5f4f1bedbe3881b52ebcd047cbb06d
ASAD ANSARI <mdasadansari11223@gmail.com>
2024-10-27 19:07:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+
+
+
+
+//    ASSIGNMENT 06 RANDOM PASSWORD GENRATOR    //
+
+var str = "ABCDEFGHDGHD2467abcdefijklmn<?%>*&";
+for (var i = 0; i < str.length; i++) {
+
+  var random = Math.floor(Math.random() * str.length)
+  document.write(str[random])
+
+}
+
+ var str = ["A","B","C","D","@"];
+
+for (var i = 0; i < str.length; i++) {
+
+  var random = Math.floor(Math.random() * str.length)
+  document.write(str[random])
+
+}
+
+
+var str = prompt("Enter any 4 or 5 characters from which the password should be made");
+
+for (var i = 0; i < str.length; i++) {
+
+  var random = Math.floor(Math.random() * str.length)
+  document.write(str[random])
+
+}
